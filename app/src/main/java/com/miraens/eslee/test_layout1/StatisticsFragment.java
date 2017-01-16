bbe6d4d304a5591b0691407ac786fe68187192b9
sw8657 <sw8657@nate.com>
2017-01-16 17:37:49 +0900

    차트 페이지 추가

--- app/src/main/java/com/miraens/eslee/test_layout1/StatisticsFragment.java
+++ app/src/main/java/com/miraens/eslee/test_layout1/StatisticsFragment.java
@@ -5,6 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 
 public class StatisticsFragment extends Fragment {
@@ -16,6 +26,10 @@ public class StatisticsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    private LineChart mLineChart;
+
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -41,11 +55,61 @@ public class StatisticsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_statistics, container, false);
         getActivity().setTitle("Statistics Fragment");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        Button btnChartWeekly = (Button) view.findViewById(R.id.chart_weekly_button);
+        Button btnChartMonthly = (Button) view.findViewById(R.id.chart_monthly_button);
+        mLineChart = (LineChart) view.findViewById(R.id.chart);
+
+        // 버튼 이벤트 핸들러 생성
+        btnChartWeekly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"주간",Toast.LENGTH_SHORT).show();
+                CreateChartView();
+            }
+        });
+
+        btnChartMonthly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"월간",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
+
+    private void CreateChartView(){
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(2f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        LineData data = new LineData(labels, dataset);
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        dataset.setDrawCubic(true);
+        dataset.setDrawFilled(true); //선아래로 색상표시
+
+        mLineChart.setData(data);
+        mLineChart.animateY(2000);
+    }
+
 
 }
